da969ee7c35e17f113177cb28366d8e503570185
Wcao23 <caoways@gmail.com>
2025-11-29 15:56:23 -0500

    added read back up file and extract (Rough copy)

--- restore.py
+++ restore.py
@@ -4,11 +4,15 @@
 Author: Wayson Cao
 Student number: 130300239
 '''
-import subprocess
+
 import tarfile
-import zipfile
 
 def restore(backup_file, restore_path):
     #Purpose: To restore a compressed backup file
     #Brainstorming: use subprocess.open like a1 to run tar cmd or use tarfile  or zipfile
+
+    tar = tarfile.open(backup_file, 'r') # Reads backup file
+    tar.extractall(path=restore_path) # extract tar (backup file), path= where the backup file gets extracted to
+
+
      
