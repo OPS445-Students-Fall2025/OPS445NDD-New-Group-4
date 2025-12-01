dcfbb2bbd9958a8518906a7e17d7d78e23cc3f70
Wcao23 <caoways@gmail.com>
2025-12-01 17:27:21 -0500

    initial exclude and idea

---
+++ exclude.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+'''
+Author: Wayson Cao
+Student number: 130300239
+'''
+
+def exclude(par_dir, sub_dir):
+    # It should ask for parent directory, subdirectory to exclude
