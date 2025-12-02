63085d4e797de2a951f4ff5132120d172ab386e3
agnestra <agnestra@icloud.com>
2025-12-02 18:25:51 -0500

    fixed indentation

--- assignment2.py
+++ assignment2.py
@@ -4,14 +4,13 @@ OPS445 Assignment2
 Author: Agnestra Mahat
 Author ID:128939238
 Program: assignment2.py
-
 '''
 
 # Import necessary functions
 from exclude import exclude
 from compress1 import compress
 from restore import restore
-from args_parse import parse_command_args  
+from args_parse import parse_command_args
 import os
 
 # Main Program
@@ -19,7 +18,7 @@ if __name__ == "__main__":
     # Get the user input in the command line
     args = parse_command_args()
 
-    # If the compress option is used in the command line argument 
+    # If the compress option is used in the command line argument
     if args.action == "compress":
         target_dir = args.target  # sets the target directory to compress
 
@@ -28,16 +27,19 @@ if __name__ == "__main__":
             print(f"Error: Target directory '{target_dir}' does not exist.")
             exit(1)
 
-# Make an empty list to store the names of subfolders
-subdirs = []
-# Loop through every item inside the target directory
-for item in os.listdir(target_dir):
-    # creates the full pathname for each item 
-    full_path = os.path.join(target_dir, item)
-    # checks if this item is a subdirectory
-    if os.path.isdir(full_path):
-        # If it IS a folder, add its name to our subdirs list
-        subdirs.append(item)
+        # Make an empty list to store the names of subfolders
+        subdirs = []
+
+        # Loop through every item inside the target directory
+        for item in os.listdir(target_dir):
+            # creates the full pathname for each item
+            full_path = os.path.join(target_dir, item)
+
+            # checks if this item is a subdirectory
+            if os.path.isdir(full_path):
+                # If it IS a folder, add its name to our subdirs list
+                subdirs.append(item)
+
         # checks if the some directories were required to be skipped
         if args.exclude:
             # based on the exclude condition provided, filters the sub_dirs
@@ -58,7 +60,7 @@ for item in os.listdir(target_dir):
         target_file = args.target  # backup file to restore
         restore_path = args.output  # path  to restore the files
 
-        # checks if the backup fole exists if not prints an error message
+        # checks if the backup file exists if not prints an error message
         if not os.path.isfile(target_file):
             print(f"Error: Backup file '{target_file}' does not exist.")
             exit(1)
