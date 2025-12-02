1fbb1ff7f62211d5e42aa1bc8007f323bd2672ec
Wcao23 <caoways@gmail.com>
2025-12-02 00:48:51 -0500

    final product

--- exclude.py
+++ exclude.py
@@ -5,8 +5,12 @@ Author: Wayson Cao
 Student number: 130300239
 '''
 
-# Checks a list of exclude instead from target directory, if sub directory have nothing return the list
-def exclude(file_list, sub_dir):
-    if sub_dir == None:
-        return file_list
+def exclude(subdirectories, subdir_to_exclude): # If there is no subdirectory to exclude, leave it be
+    if subdir_to_exclude == None:
+        return subdirectories
 
+    subdir_not_excluded = []
+    for subdir in subdirectories: # For every subdirectory in subdirectories
+        if subdir_to_exclude not in subdir: # only selects directories that are not excluded
+            subdir_not_excluded.append(subdir) # Add that subdir to not excluded subdirectory list
+    return subdir_not_excluded # Return a list of subdirectories to compress
