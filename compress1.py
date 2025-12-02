a686fefb5c5ec7381c3c8545c8b187ec6b091fe5
Sashank Kharal <skharal3@myseneca.ca>
2025-12-02 02:00:40 -0500

    added compress function to work with exclude

--- compress1.py
+++ compress1.py
@@ -3,21 +3,36 @@
 Compress.py
 Sashank Kharal
 Assignment 2
+id=137379236
+compress subdirectories with exclude
 """
 
-import os
+import os 
 import tarfile
 
-def compress(target_dir, output_file): # compress all files in a folder into a tar.gz archive
+def compress(target_dir, output_file, subdirs_to_compress=None):
+"""
+Compress all subdirectories in target_dir into a tar.gz archive.
+"""
 
-    tar = tarfile.open(output_file, "w:gz") # open the tar.gx file manually
+#open the tar.gz file for writing manually
+tar = tarfile.open(output_file, "w:gz")
 
-    for item in os.listdir(target_dir): #loop through all itemin target directory
-        full_path = os.path.join(target_dir, item) #get the full path of the item
-        
-        if os.path.isfile(full_path):  # it only include files
-            tar.add(full_path, arcname=item) # add file to archive with just the filename
+# If no filtered list is provided it get all subdirectories in target_dir
+if subdirs_to_compress is None:
+    subdirs_to_compress = [d for d in os.listdir(target_dir) if os.path.isdir(os.path.join(target_dir, d))]
 
-    tar.close() #close the tar archive
+# it print the list of subdirectories that will be compressed
+print("Subdirectories being compressed:", subdirs_to_compress)
 
-    print("Compression completed!")
+# loop through each subdirectory and add it to the tar archive
+for subdir in subdirs_to_compress:
+    # get the full path of the subdirectory
+    full_path = os.path.join(target_dir, subdir)
+    # add the subdirectory to the archive arcname=subdir and keeps only the folder name in the archive
+    tar.add(full_path, arcname=subdir)
+
+# close the tar archive to save changes
+tar.close()
+# print confirmation that compression is done
+print("Compression completed!")
